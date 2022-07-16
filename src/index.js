d640fc7f52e3896a65e22cb421633997c87c3ca8
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-07-16 17:14:57 +0300

    feat: Done Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let example = bracketsConfig.flat().join('');
+  let arr = Array.from(str);
+  if (str.length % 2 !==0) {
+    return false;
+  } else {
+    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < example.length; i += 2) {
+      for (let j = 0; j < str.length; j += 1) {
+        if (str.slice(j, j + 2) === example.slice(i, i + 2)) {
+          str = (str.substring(0, j)).concat(str.substring(j + 2, str.length));
+        }
+      }
+      }
+    };
+    
+  if (str.length === 0) {
+      return true;
+    } else if (str.length === 2){
+      for (let i = 0; i < example.length; i += 2) {
+        if (str === example.slice(i, i + 2)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+};
